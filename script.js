eafd15da719cfb0590fed96a86b7d61233dda602
ulysses316 <ulises316@live.com.mx>
2018-03-08 17:47:03 -0600

    [add][update] se agrego la opcion de poder añadir marcadores al mapa, y se corrigieron errores en la importacion del script en el html

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+/*Creamos una variable llamada map y una funcion llamada initMap que no recibe ningun parametro
+a nuestra variable map le decimos que sera un nuevo objeto de la funcion google.maps.Map que recibe como parametros
+un elemento div del html, el cual se lo pasaremos por el ID, un objetos llamado center, que recibira la latitudes y longitudes
+del lugar del cual queremos mostrar el mapa, y el zoom que le meteremos al mapa
+*/
 var map;
+var marcador = [
+  {lat:19.4751611, lng:-99.0468698},
+  {lan:19.4856331,lng:-99.0511556}
+]
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 19.4751611, lng: -99.0468698},//CAMPO OBLIGATORIO
+    zoom: 8  // CAMPO OBLIGATORIO
   });
+
+  var marker = new google.maps.Marker({
+    position: marcador,
+    map: map,
+    //icon:'mascara.png'
+  });
+
+
 }
