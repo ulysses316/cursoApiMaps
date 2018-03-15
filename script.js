fbbe9044e44fb098b7700c3bf0c82bc57cdbf7cf
ulysses316 <ulises316@live.com.mx>
2018-03-14 18:27:02 -0600

    [add], consumo de la API geo code de google maps API, creamos la funcion geoCode y recibimos el objetos json con la respuesta al servicio web que realizamos

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function initMap() {
     center: {lat: 19.4751611, lng: -99.0468698},//CAMPO OBLIGATORIO
     zoom: 8  // CAMPO OBLIGATORIO
   });
+  //{lat:47.608013, lng:-122.335167}
 
 /*La funcion agregarMarcador fue creada por nosotros para poder realizar todo el proceso de agregar marcadores
 de una manera mas simple y no tener que andar copiando y pegando todo el codigo de adentro una y otra vez para agregar marcadores*/
@@ -28,6 +29,28 @@ function agregarMarcador(cordenadas, flag){
 //Asi es como invocamos nuestra funcion de marcadores
 agregarMarcador({lat: 19.4751611, lng: -99.0468698},);
 agregarMarcador({lat: 19.6021021, lng: -99.0631558},);
-
-
 }
+// ===================================API GEOCODING======================================= //
+//Creaos la funcion geoCode para encapsular el servicio de google geo code
+function geoCode(){
+  //usasmos la funcion axios con su parametro get (en la que hacemos peticion get "obviously") y le
+  // pasamos nuestra URL de la que consumiremos el servicio, y concatenamos por medio de objetos
+  //los parametros
+  axios.get('https://maps.googleapis.com/maps/api/geocode/json?',{
+  params:{
+    address:'CDMX',
+    key:'AIzaSyDmlJky0d55y9q9i7XY9oyIvxJoYfUv5_I'
+  }
+  })
+  .then(function(response) {      // then es lo que hara si resive la respuesta correctamente
+    //imprimir respuesta total    // catcha lo que realizara en caso de que halla algun error
+    console.log(response);
+    //imprimir respuesta address_components
+    console.log(response.data.results[0].address_components[0].long_name)
+  })
+  .catch(function (error){
+    console.log(error)
+  })
+}
+
+geoCode()    // ejecutamos la funcion 
